36da746d4b7f9e66c55b8acf7f3bceaf6f203ba8
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-02-24 16:13:35 -0600

    {File, Href and Text} loops updated

--- scripts/path.js
+++ scripts/path.js
@@ -3,47 +3,23 @@ const markdownLinkExtractor = require('markdown-link-extractor');
 const validate = require('../scripts/validate.js');
 
 module.exports = {
-    readPath: (file) => {
+    readPath: (path) => {
 
-        return fs.readFileSync(file).toString();
+        return fs.readFileSync(path).toString();
     },
-    textMdArray: (str) => {
+    getLabelLinks: (parsedFile) => {
 
-        let labelLinksArray = str.match(/\[.+\w+(\w|\W)\]/g);
-        return labelLinksArray;
-    }/*,
-    showLinks: (labelLinks) => {
+        return parsedFile.match(/\[.+\w+(\w|\W)\]/g);
+    },
+    showLinks: (path, parsedFile, labelLinks ) => {
 
-        /*file = path.basename(process.argv[2]);
-        markdown = fs.readFileSync(process.argv[2]).toString();
-        textResult = markdown.match(/\[.+\w+(\w|\W)\]/g);
-        links = markdownLinkExtractor(str)
+        let links = markdownLinkExtractor(parsedFile);        
+        let pathInfoArray = [];
         
-            let pathInfoResult = {
-                File: ' ',
-                Href: ' ',
-                Text: ' '
-            };
+            links.forEach((link, index) => { 
             
-            let counter = 0;
-
-            for (let i = 0; i < labelLinks.length; i++) { 
-        
-            links.forEach((link) => {  
-                
-                if (counter >= labelLinks.length){
-
-                } else {
-                    
-                pathInfoResult.File = file;
-                pathInfoResult.Href = link;
-                pathInfoResult.Text = textResult[counter];
-
-                counter++;
-                console.log(pathInfoResult);
-                }
-                
+                pathInfoArray.push({File: path, Href: link, Text: labelLinks[index]});
             });
-        }
-    } */
+            return pathInfoArray;
+    } 
 }

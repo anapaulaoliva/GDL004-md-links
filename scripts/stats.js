1da59432f48598272ab03506b85bda7e5e3dc1b1
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-03-10 22:15:28 -0600

    Stats.js

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,11 +1,9 @@
-const fs = require('fs');
-//const pathJS = require('../scripts/path.js');
 
 module.exports = {
-    showStats: (markdown, textResult) => {
-            markdown = fs.readFileSync(process.argv[2]).toString();
-            textResult = markdown.match(/\[.+\w+(\w|\W)\]/g);
-    
-            console.log('Total Links: ' + textResult.length);
+    showStats: (array) => {
+        let totalLinks = {
+            'Total Links': array.length
+        }
+        return totalLinks;
         }
 }
